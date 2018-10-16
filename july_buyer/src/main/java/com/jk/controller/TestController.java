ffb73c5a8cf4de11b5edd4f7a2b1b6f3fced1f5a
zhanglu95 <804901537@qq.com>
2018-10-16 15:20:34 +0800

    zll

--- july_buyer/src/main/java/com/jk/controller/TestController.java
+++ july_buyer/src/main/java/com/jk/controller/TestController.java
@@ -1,10 +1,19 @@
 package com.jk.controller;
 
+import com.jk.buyer.service.BuyerService;
 import com.jk.model.Test;
+import com.jk.seller.service.SellerServiceApi;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-public class TestController {
+@RestController
+public class TestController implements BuyerService {
 
-    public void sele(Test test){
-
+    @Override
+    public Test queryOrderById(String testId) {
+        System.out.println(testId);
+        Test test = new Test();
+        test.setId("234234");
+        return test;
     }
 }
