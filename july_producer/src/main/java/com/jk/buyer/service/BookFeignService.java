dcc55a9054590faa8096ebb659dfff41955ddf44
郝杨 <524163920@qq.com>
2018-10-17 19:05:39 +0800

    丰满框架

--- july_producer/src/main/java/com/jk/buyer/service/BookFeignService.java
+++
@@ -1,8 +0,0 @@
-package com.jk.buyer.service;
-
-import com.jk.service.BuyService;
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient("service-buyer")
-public interface BookFeignService extends BuyService {
-}
