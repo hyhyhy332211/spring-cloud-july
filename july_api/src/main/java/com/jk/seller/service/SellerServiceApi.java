4147a443b96263f49ac64550e289ad8495e4f4e8
zll <804901537@qq.com>
2018-10-20 13:47:16 +0800

    zhanglu

--- july_api/src/main/java/com/jk/seller/service/SellerServiceApi.java
+++ july_api/src/main/java/com/jk/seller/service/SellerServiceApi.java
@@ -1,11 +1,11 @@
 package com.jk.seller.service;
 
-import com.jk.model.zll.tree.Tree;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.jk.model.zll.Tree;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Map;
 
 @RequestMapping("seller")
 public interface SellerServiceApi {
@@ -13,6 +13,33 @@ public interface SellerServiceApi {
     @GetMapping("queryList")
     String queryList(@RequestParam("userId") String userId);
 
+
+    /**
+     * 初始化树
+     * @return
+     */
     @GetMapping("getTreeList")
     List<Tree> getTreeList();
+
+    /**
+     * 查询定制比价管理(服务商)列表
+     * @return
+     */
+    @GetMapping("getComparisonList")
+    Map<String,Object> getComparisonList(@RequestParam(value = "customProduct") String customProduct);
+
+    /**
+     * 查询材料单
+     */
+    @GetMapping("getMaterialList")
+    Map<String, Object> getMaterialList(@RequestParam(value = "material") String material);
+
+    /**
+     * 待接单状态修改为待出图
+     */
+    @PutMapping("updateNoSingleStatus")
+    void updateNoSingleStatus(@RequestParam("productID") String productID);
+
+    @PostMapping("uploadFile")
+    String Upload(@RequestParam(value = "file") MultipartFile file);
 }
